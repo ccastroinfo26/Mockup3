9e1d84151174ffee042162a55cfe7005e436b86e
CCASTROINFO26 <claitonycastro@gmail.com>
2020-01-13 11:52:47 -0300

    menu quase pronto

---
+++ app/src/main/java/com/example/mockup/splashScreen.java
@@ -0,0 +1,24 @@
+package com.example.mockup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() { }
+            }, 2000);
+            Intent intent = new Intent(splashScreen.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+}
